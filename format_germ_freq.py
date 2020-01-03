4af31b2682faa46d4d6ed5b330dc1660a00c2433
Nathan Glenn <garfieldnate@gmail.com>
2020-01-03 15:57:22 +0100

    More parseable output from format_germ_freq.py
    join lines in sections that belong together (as single sentences). Now individual pieces of information are contained in individual elements.

--- format_germ_freq.py
+++ format_germ_freq.py
@@ -36,13 +36,16 @@ def process_page(line_num, page_num, lines):
     split_index = find_column_split_index(stripped_lines)
     # print(f"Page {page_num} (through line {line_num}) is split at char {split_index}")
     single_col_lines = split_cols(stripped_lines, split_index)
+    # for line in single_col_lines:
+    #     print(line)
+    # sys.exit()
     # final_lines = [l.rstrip() for l in single_col_lines]
     output_new_lines(single_col_lines)
 
 
 def find_column_split_index(lines):
 
-    """Finds the character index at which the input lines containing two colllumns of text are split.
+    """Finds the character index at which the input lines containing two collumns of text are split.
     Scans all lines on the page from right to find first a "•" (bullet point) and then a number (the frequency ranking). 
     After the ranking is found, continues to scan left to find an index at which all lines contain whitespace. This index 
     is returned."""
@@ -63,18 +66,23 @@ def find_column_split_index(lines):
 
                 if line[i] == "•":
                     bullet_found = True
+                    ranking_found = False
+                    # print(f'found bullet at {i}; reset ranking_found to false')
                     raise ContinueCharIndexLoop
 
                 if bullet_found and line[i].isdigit():
                     ranking_found = True
+                    # necessary because sometimes the bullets aren't aligned
                     all_whitespace = False
-                    raise ContinueCharIndexLoop
+                    # print(f'found digit after bullet at {i} in line {line}')
 
                 if ranking_found and not line[i].isspace():
                     all_whitespace = False
+
         except ContinueCharIndexLoop:
             pass
         if ranking_found and all_whitespace:
+            # print(f'ranking found and all whitespace at {i}')
             return i
 
     raise ValueError("Could not find split index")
@@ -93,38 +101,60 @@ def split_cols(lines, index):
     return left_col + right_col
 
 
+# TODO: This is pretty silly. If further work is needed, just parse out the entries properly and then print the HTML in a separate step.
 def output_new_lines(lines):
     print('<!DOCTYPE html>')
     print('<head><meta charset="utf-8"></head>')
     print('<style>body { font-size: 60pt; }</style>')
     in_entry = False
+    close_prev = ''
     for line in lines:
         if not line:
             continue
         if (match := headword_pattern.match(line)):
+            if close_prev:
+                print(close_prev)
+                close_prev = ''
             if in_entry:
                 print("</div>")
+            print(NEXT_SCREEN)
             in_entry = True
             print("<div class='entry'>")
-            output_headword(match)
+            close_prev = output_headword(match)
         elif (match := polysemous_headword_pattern.match(line)):
-            output_single_meaning(match)
+            if close_prev:
+                print(close_prev)
+                close_prev = ''
+            close_prev = output_single_meaning(match)
         elif (match := secondary_meaning_pattern.match(line)):
+            if close_prev:
+                print(close_prev)
+                close_prev = ''
             output_secondary_meaning(match)
         elif (match := frequency_score_pattern.match(line)):
+            if close_prev:
+                print(close_prev)
+                close_prev = ''
             output_frequency_score(match)
+        elif line.lstrip()[0] == "•":
+            if close_prev:
+                print(close_prev)
+                close_prev = ''
+            close_prev = output_example(line.lstrip())
         else:
-            print('<br/>')
-            print(f'<h2>{line}</h2>')
+            print(line.lstrip())
+    print(close_prev)
     print("</div>")
 
 def output_headword(match):
-    print(NEXT_SCREEN)
-    print('<h1>' + '<span class="rank">' + match.group('rank') + '</span> ' + '<span class="headword">' + match.group('headword') + '</span>' + '</h1>')
+    print('<h1>' + '<span class="rank">' + match.group('rank') + '</span> ' + '<span class="headword">' + match.group('headword'))
     if(pos := match.group('pos')):
+        print('</span>' + '</h1>')
         # plenty of space so we can quiz ourselves before scrolling down
         print(NEXT_SCREEN)
-        print('<h1>' + get_pos_html(pos) + '<span class="meaning">' + match.group('meaning') + '</span>' + '</h1>')
+        print('<h1>' + get_pos_html(pos) + '<span class="meaning">' + match.group('meaning'))
+
+    return '</span>' + '</h1>'
 
 
 def get_pos_html(pos):
@@ -137,15 +167,23 @@ def get_pos_html(pos):
 def output_single_meaning(match):
     print(NEXT_SCREEN)
 
-    print('<h1>' + '<span class="index">' + match.group('index') + '</span> ' + get_pos_html(match.group('pos')) + '<span class="meaning">' + match.group('meaning') + '</span>' + '</h1>')
+    print('<h1>' + '<span class="index">' + match.group('index') + '</span> ' + get_pos_html(match.group('pos')) + '<span class="meaning">' + match.group('meaning'))
+    return  '</span>' + '</h1>'
 
 
 def output_secondary_meaning(match):
-    print('<h1>' + '<span class="meaning">' + match.group(0) + '</span>' + '</h1>')
+    print('<h1>' + '<span class="meaning">' + match.group(0).lstrip())
+    return '</span>' + '</h1>'
 
 
 def output_frequency_score(match):
     print('<br/>' + '<span class="score">' + match.group('score') + '</span>')
+    return None
+
+
+def output_example(line):
+    print(f'<br/><span class="example">{line}')
+    return '</span>'
 
 def main(argv):
     if len(argv) != 2:
